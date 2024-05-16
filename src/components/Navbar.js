e0e04e00aed84f14ae8ed51289b9790bc2fc9fff
inwooCS <inwoo@hawaii.edu>
2024-05-16 12:51:06 -1000

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,88 @@
-import React from 'react';
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
-// import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Navbar, Nav, NavDropdown, Container, Image } from 'react-bootstrap';
 
-import { Image } from 'react-bootstrap';
 import jbchusalogo from '../images/jbchusa-logo.png';
 
-class NavBar extends React.Component {
-  render() {
+// Custom hook to manage dropdown state
+function useDropdownState(initialState = false) {
+  const [isOpen, setIsOpen] = useState(initialState);
+
+  const openDropdown = () => setIsOpen(true);
+  const closeDropdown = () => setIsOpen(false);
+
+  return { isOpen, openDropdown, closeDropdown };
+}
+
+// Dropdown component
+function Dropdown({ title, items }) {
+  const { isOpen, openDropdown, closeDropdown } = useDropdownState(false);
+  const dropDirection = title === "Our Mission" ? "end" : "start"; // Conditionally set drop direction
+
   return (
-    <div> 
-    <Navbar bg="dark" variant="dark" expand="lg" >
-      <Container fluid >
-        {/* <Navbar.Brand href="/">Jesus Baptist Hawaii Church</Navbar.Brand> */}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto"> {/* Center the image */}
-            <Image src={jbchusalogo} alt="Honolulu" fluid style={{ width: '50%', height: 'auto' }} />
-          </Nav>
-          <Nav className="ms-auto" style={{fontSize:"20px"}}> {/* Align NavLinks to the right */}
-            <Nav.Link href="/"> Home</Nav.Link>
-            <Nav.Link href="/sermon"> Sermon </Nav.Link>
-            <Nav.Link href="/fellowship"> Fellowship </Nav.Link>
-            <Nav.Link href="/contact"> Contact </Nav.Link>
-            <NavDropdown title="Our Mission" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">USA Conference</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Life World Mission</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <NavDropdown
+      title={title}
+      id="basic-nav-dropdown"
+      show={isOpen}
+      onMouseEnter={openDropdown}
+      onMouseLeave={closeDropdown}
+      align={dropDirection} // Set drop direction based on title
+    >
+      {items.map((item, index) => (
+        <NavDropdown.Item key={index} href={item.url}>
+          {item.title}
+        </NavDropdown.Item>
+      ))}
+    </NavDropdown>
+  );
+}
+
+// Navbar component
+function NavBar() {
+  return (
+    <div>
+      <Navbar bg="white" expand="lg">
+        <Container fluid>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto">
+              <Image src={jbchusalogo} alt="Honolulu" fluid style={{ width: '50%', height: 'auto' }} />
+            </Nav>
+            <Nav className="ms-auto" style={{ fontSize: '20px' }}>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Dropdown
+                title="About Us"
+                items={[
+                  { title: 'Staff And Leadership', url: '/churchschool' },
+                  { title: 'What We Believe', url: 'https://youtube.com' },
+                  { title: 'What Is Christianity?', url: '#action/3' },
+                  { title: "Contact Us", url: '#action/4' }
+                ]}
+              />              
+              <Nav.Link href="/newtochurch">New&nbsp;To&nbsp;Church?</Nav.Link>
+              <Nav.Link href="/churchlife">Church&nbsp;Life</Nav.Link>
+              <Dropdown
+                title="Ministries"
+                items={[
+                  { title: 'Church School', url: '/churchschool' },
+                  { title: 'Youth Group', url: 'https://youtube.com' },
+                  { title: 'Young Adult Group', url: '#action/3' },
+                  { title: "Mother's Group", url: '#action/4' },
+                  { title: "Father's Group", url: '#action/5' }
+                ]}
+              />
+              <Dropdown 
+                title="Our Mission" 
+                items={[
+                  { title: 'USA Conference', url: 'https://jbchusa.org/home' },
+                  { title: 'Life World Mission', url: 'https://www.jbch.org/kor/index.php'}
+                ]} 
+              />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-    )
-  }
+  );
 }
 
 export default NavBar;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,33 +1,38 @@
 
-import React, {Component} from 'react';
+import React from 'react';
 import honoluluImage from '../images/honolulu.jpeg';
-import { Button, Alert, Breadcrumb, Card, Image, Container } from 'react-bootstrap';
-
+import { Button, Image, Container} from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
 
 class Landing extends React.Component {
     render() {
         return (
             <div>
-            {/* The container is assigned fluid to fit the image to page, the class name removes the trailing white spaces. */}
-            <div>
-         
-            <Image src={honoluluImage} alt="Honolulu" fluid style={{ width: '100%', height: 'auto' }}/> 
-            <div className="text-light overlay-text-container position-absolute top-50 start-50 translate-middle">
-                <h1>Welcome to Honolulu</h1>
-                <p>Discover the beauty of Hawaii</p>
-            </div>
-            </div>
-            
-            <text className='fontSize: 30px'>Come and see.</text>
-            <h3 style={{ fontSize: '2.0rem', marginTop: '-0.5rem' }}>John 1:46</h3>
-            <div style={{ marginRight: '.5rem', marginTop: '1rem' }}>
-            <Button className='rounded-pill' variant="primary" size="lg" style={{ marginRight: '.5rem', padding: '0.5rem 1rem' }}>
-                <p>Plan a Visit</p>
-            </Button>{' '}
-            <Button  className='rounded-pill text-white border-white' variant="outline-secondary" size="lg" >
-                <text size='md'>Watch Live</text>
-            </Button>
-            </div>
+                <Carousel>
+                    <Carousel.Item>
+                        <Image src={honoluluImage} alt="Honolulu" fluid style={{ width: '100%', maxHeight: '500px', objectFit: 'cover' }} />
+                        <Carousel.Caption style={{ margin: '120px',textAlign: 'center', color: 'white'}}>
+                            <h1 style={{fontSize: '50px' }}>About Us</h1>
+                            <p style={{fontSize: '36px' }}>Explore our church</p>
+                            <Button variant="primary" size="lg">Read more</Button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Image src={honoluluImage} alt="Honolulu" fluid style={{ width: '100%', maxHeight: '500px', objectFit: 'cover' }} />
+                        <Carousel.Caption style={{ margin: '120px',textAlign: 'center', color: 'white'}}>
+                            <h1 style={{fontSize: '50px' }}>Sermon catch-up</h1>
+                            <p style={{fontSize: '36px' }}>Listen at home</p>
+                            <Button variant="primary" size="lg">Sermon series</Button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+                <div>
+                    <Container>
+                        <h1 >twest</h1>
+                    </Container>
+                    {/* #abc9fb */}
+                    <h1> hello world</h1>
+                </div>
             </div>
         );
     }
